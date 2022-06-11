be1ef8609169a6450083af34f332444b3901d065
ccml <claude.cato@proximus.be>
2022-06-11 21:54:48 +0200

    @Value

--- src/main/java/com/mycompany/invoice/service/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoice/service/InvoiceServicePrefix.java
@@ -3,9 +3,12 @@ package com.mycompany.invoice.service;
 import com.mycompany.invoice.entity.Invoice;
 import com.mycompany.invoice.repository.InvoiceRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
+    @Value("${invoice.lastNumber}")
     private long lastNumber;
+    @Value("${invoice.prefix}")
     private String prefix;
 
     public long getLastNumber() {
