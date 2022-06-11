093a0a9c6f092020d48e1ac44b26a7c5a1997a0e
ccml <claude.cato@proximus.be>
2022-06-11 22:54:52 +0200

    SpringBootApplication

--- src/main/java/com/mycompany/invoice/App.java
+++ src/main/java/com/mycompany/invoice/App.java
@@ -2,8 +2,8 @@ package com.mycompany.invoice;
 
 
 import com.mycompany.invoice.controller.InvoiceControllerInterface;
-import com.mycompany.invoice.service.InvoiceServiceInterface;
-import com.mycompany.invoice.service.prefix.InvoiceServicePrefix;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
@@ -11,25 +11,16 @@ import org.springframework.context.annotation.*;
  * Hello world!
  *
  */
-@Configuration
-@ComponentScan(basePackages = {
-        "com.mycompany.invoice.controller.web",
-        /* "com.mycompany.invoice.service.prefix", */
-        "com.mycompany.invoice.repository.database"
-})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
 
         InvoiceControllerInterface invoiceController = (InvoiceControllerInterface) context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoice();
     }
 
-    @Bean
-    public InvoiceServiceInterface configureInvoiceService() {
-        return new InvoiceServicePrefix();
-    }
 }
--- src/main/java/com/mycompany/invoice/service/prefix/InvoiceServicePrefix.java
+++ src/main/java/com/mycompany/invoice/service/prefix/InvoiceServicePrefix.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
     @Value("${invoice.lastNumber}")
     private long lastNumber;
