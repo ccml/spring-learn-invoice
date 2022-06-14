0003b42ddab66ad602cb47846f308871c34a1658
ccml <claude.cato@proximus.be>
2022-06-14 17:37:54 +0200

    formulaire

--- src/main/java/com/mycompany/invoice/core/controller/InvoiceControllerInterface.java
+++ src/main/java/com/mycompany/invoice/core/controller/InvoiceControllerInterface.java
@@ -1,8 +1,9 @@
 package com.mycompany.invoice.core.controller;
 
+import com.mycompany.invoice.core.entity.Invoice;
 import com.mycompany.invoice.core.service.InvoiceServiceInterface;
 
 public interface InvoiceControllerInterface {
     void setInvoiceService(InvoiceServiceInterface invoiceService);
-    void createInvoice();
+    String createInvoice(Invoice invoice);
 }
--- src/main/java/com/mycompany/invoice/core/controller/keyboard/InvoiceControllerKeyboard.java
+++ src/main/java/com/mycompany/invoice/core/controller/keyboard/InvoiceControllerKeyboard.java
@@ -21,12 +21,13 @@ public class InvoiceControllerKeyboard implements InvoiceControllerInterface {
         this.invoiceService = invoiceService;
     }
 
-    public void createInvoice() {
+    public String createInvoice(Invoice invoice) {
         System.out.println( "What is the customer name" );
         Scanner sc = new Scanner(System.in);
         String customerName = sc.nextLine();
-        Invoice invoice = new Invoice();
+        invoice = new Invoice();
         invoice.setCustomerName(customerName);
         invoiceService.createInvoice(invoice);
+        return null;
     }
 }
